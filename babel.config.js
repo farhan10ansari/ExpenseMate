db0c66a12a49f8b3798a046a15fc5bbc796e1993
farhan10ansari <farhan10ansari@gmail.com>
2025-08-23 19:09:38 +0530

    enable react compiler

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,19 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      [
+        'babel-preset-expo',
+        {
+          'react-compiler': {
+            sources: filename => {
+              // Match file names to include in the React Compiler.
+              return filename.includes('src/path/to/dir');
+            },
+          },
+        },
+      ],
+    ],
     plugins: [["inline-import", { "extensions": [".sql"] }]],
     env: {
       production: {
